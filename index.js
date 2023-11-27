ec075f90772ad3c83856a85dd6da7103fb2658dd
Motinha <guiguimota2017@gmail.com>
2023-11-27 20:22:32 -0300

    primeira execução do servidor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require("express")
 
 const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Olá, viadinho!")
+})
+
+app.listen(3000, () => {
+    console.log("Trabalho do Marcio rodando na porta 3000!")
+})
