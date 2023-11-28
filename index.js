5deff6fb1094e2b4dea62cc63ad4976d19a8ace2
Motinha <guiguimota2017@gmail.com>
2023-11-27 23:22:08 -0300

    iniciando leitura dos dados do banco de dados

--- index.js
+++ index.js
@@ -38,6 +38,23 @@ app.post('/criar', (requisicao, resposta) => {
 })
 
 app.get('/', (requisicao, resposta) => {
+    const sql = 'SELECT * FROM tarefas'
+
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dado) => {
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: dado.completa === 0 ? false : true
+            }
+        })
+
+        
+    })
     resposta.render('home')
 })
 
