52ada18158e345c273d8bea2d6e2555e45be3656
Motinha <guiguimota2017@gmail.com>
2023-11-28 00:21:45 -0300

    iniciando filtro de tarefas ativas

--- index.js
+++ index.js
@@ -75,7 +75,9 @@ app.post('/criar', (requisicao, resposta) => {
     
 })
 
-
+app.get('/ativas', (requisicao, resposta) => {
+    
+})
 
 app.get('/', (requisicao, resposta) => {
     const sql = 'SELECT * FROM tarefas'
